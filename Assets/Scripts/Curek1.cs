763f6ff0176e4bc5a382c24d1a4e7e03227e2842
Choro11 <choro11@gmail.com>
2016-01-29 22:34:14 +0100

    Pissing - curek1

---
+++ Assets/Scripts/Curek1.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Curek1 : MonoBehaviour
+{
+    void OnCollisionEnter(Collision c)
+    {
+        if (c.gameObject.tag != "Curek")
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Lulek.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lulek : MonoBehaviour
+{
+    public enum smer
+    { Naprej, Levo, Desno, Nazaj }
+
+    public smer smerCurka = smer.Naprej;
+    public GameObject curek;
+    [Range(0,300)]
+    public float moč = 200f;
+
+	//void Start ()
+    //{
+	
+	//}
+	
+
+	void Update ()
+    {
+	    if (curek != null)
+        {
+            GameObject temp = (GameObject)Instantiate(curek, gameObject.transform.position, Quaternion.identity);
+            addForce(temp.GetComponent<Rigidbody>());
+        }
+	}
+
+    void addForce(Rigidbody rb)
+    {
+        switch (smerCurka)
+        {
+            case smer.Naprej:
+                rb.AddForce(transform.forward * moč);
+                break;
+            case smer.Nazaj:
+                rb.AddForce(-transform.forward * moč);
+                break;
+            case smer.Levo:
+                rb.AddForce(transform.right * moč);
+                break;
+            case smer.Desno:
+                rb.AddForce(-transform.right * moč);
+                break;
+        }
+         
+    
+    }
+}
